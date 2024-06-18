1deb42b3340d9a0ca5b59742e7876cdc35e1396b
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-17 17:33:47 -0700

    Initial Commit

---
+++ FirebaseAuthentication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseAuthentication
+//
+//  Created by Arpit iOS Dev. on 17/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
